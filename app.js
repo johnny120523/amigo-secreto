7403d883430777cc442b87b2f50363532a610da6
Johnny Ortiz <johnnystanleyortizmondragon@gmail.com>
2025-07-30 12:39:27 -0500

    Actualiza el index

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigos() {
+    let agregar = document.getElementById('amigo').value;
+    if (agregar==='') {
+        alert('Por favor, ingrese un nombre')
+    } else {
+        
+    }
+}
